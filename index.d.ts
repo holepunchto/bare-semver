937f54be760f0d7c06ecb5e117f6d989eed393e1
Yasser Nascimento <dev@yasser.email>
2024-12-16 18:05:55 -0300

    Add TypeScript typings

---
+++ index.d.ts
@@ -0,0 +1,50 @@
+export const constants: { EQ: 1; LT: 2; LTE: 3; GT: 4; GTE: 5 }
+
+export const errors = class SemVerError extends Error {}
+
+export function satisfies(version: Version, range: Range): boolean
+
+export class Version {
+  constructor(
+    major: number,
+    minor: number,
+    patch: number,
+    opts?: { prerelease?: string[]; build?: string[] }
+  )
+
+  compare(version: Version): boolean
+
+  toString(): string
+}
+
+export namespace Version {
+  export function parse(
+    input: string,
+    state?: { position: number; partial: boolean }
+  ): Version
+
+  export function compare(a: Version, b: Version): number
+}
+
+export class Comparator {
+  constructor(operator: number, version: Version)
+
+  test(version: Version): boolean
+
+  toString(): string
+}
+
+export class Range {
+  constructor(comparators?: Comparator[][])
+
+  test(version: Version): boolean
+
+  toString(): string
+}
+
+export namespace Range {
+  export function parse(
+    input: string,
+    state?: { position: number; partial: boolean }
+  ): Range
+}
