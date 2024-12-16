acc129f0adb02bff49dbf7a39a105990e0578491
Yasser Nascimento <dev@yasser.email>
2024-12-16 16:25:45 -0300

    Switch to Prettier

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 exports.constants = require('./lib/constants')
 exports.errors = require('./lib/errors')
 
-const Version = exports.Version = require('./lib/version')
-const Range = exports.Range = require('./lib/range')
+const Version = (exports.Version = require('./lib/version'))
+const Range = (exports.Range = require('./lib/range'))
 exports.Comparator = require('./lib/comparator')
 
-exports.satisfies = function satisfies (version, range) {
+exports.satisfies = function satisfies(version, range) {
   if (typeof version === 'string') version = Version.parse(version)
   if (typeof range === 'string') range = Range.parse(range)
 
